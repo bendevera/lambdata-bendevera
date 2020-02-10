b497585fb0f19c77195f9cb20578bc6cd095feff
bendevera <ben10devera@gmail.com>
2020-02-10 13:18:18 -0700

    added train/test/val split

--- lambdata/tools.py
+++ lambdata/tools.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from sklearn.model_selection import train_test_split
 import os
 
 def test():
@@ -9,9 +10,19 @@ def df_null_report(df):
     for column in df.columns:
         print(f"{column} null count: ", df[column].isnull().sum())
 
+def train_test_val_split(df):
+    train, test = train_test_split(df, test_size=.2)
+    train, val = train_test_split(train, test_size=.2)
+    print("TRAIN/TEST/VAL Breakdown:")
+    print(f"TRAIN : {train.shape}")
+    print(f"TEST : {test.shape}")
+    print(f"VAL : {val.shape}")
+    return train, test, val
+
 if __name__ == "__main__":
     test()
     dir_path = os.path.dirname(os.path.realpath(__file__))
     TEST_CSV = os.path.join(dir_path, "test_data/books.csv")
     df = pd.read_csv(TEST_CSV)
     df_null_report(df)
+    train, test, val = train_test_val_split(df)
